24280b7d8826460fc97c8b2199000adda069e48d
Mell Stroy <mellstroy.numb@gmail.com>
2026-01-21 19:58:04 +0000

    ошибка

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'hi-tech.md',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
